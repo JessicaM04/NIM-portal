4c2db63163b1f8cca01878e80f2fce237f8bc49f
RKH1988 <regettaspearman@gmail.com>
2022-11-01 20:22:22 -0400

    added more changes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const Donor = require('./Donor');
+const Comment = require('./Comment');
 
-module.exports = { User, Thought };
+module.exports = { Donor, Comment };
