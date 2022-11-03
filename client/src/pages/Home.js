ad62c0de77ec9c316969761ad0376ed1ba44cc9a
RKH1988 <regettaspearman@gmail.com>
2022-11-02 21:16:24 -0400

    added source folder components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>{/* PRINT COMMENT LIST */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
