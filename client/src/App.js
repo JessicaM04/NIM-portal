aa21c056ca9022c3ae9e877c1746ce232e1da93b
RKH1988 <regettaspearman@gmail.com>
2022-11-02 21:34:10 -0400

    updated to add graphQL and Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,15 +6,26 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
